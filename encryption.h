1d86c271f0cd0392560eee55326cad2a3f96def0
Shrinav Loka <108443554+lokashrinav@users.noreply.github.com>
2025-03-03 03:00:48 -0500

    Create encryption.h

---
+++ encryption.h
@@ -0,0 +1,14 @@
+#ifndef ENC_H
+#define ENC_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+char* en(const char* a, const char* b);
+char* de(const char* a, const char* b);
+void fr(char* a);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
